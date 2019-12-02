bc10b2198b10e168d92167ba7ee902b4ccc1bff5
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2019-12-02 13:06:04 -0800

    Update gis-colab.sh

--- gis-colab.sh
+++ gis-colab.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-# Install Miniconda
-
-echo "Installing Miniconda"
-
-# Download installer for Linux 64-bit machine (another shell script)
-
-wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh
-
-# Make the installer executable as a program
-
-chmod +x Miniconda3-4.5.4-Linux-x86_64.sh
-
-# Run the installer program
-
-bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local
-
 # Install geospatial packages
 
 echo "Installing GIS libraries"
 
-# Use conda package manager to install geospatial Python libraries (and a visualization library) from conda-forge channel
-
-conda install -y --prefix /usr/local -c conda-forge  geopandas rasterstats matplotlib
+apt update
+apt upgrade
+apt install gdal-bin python-gdal python3-gdal 
+# Install rtree - Geopandas requirment
+apt install python3-rtree 
+# Install Geopandas
+pip install git+git://github.com/geopandas/geopandas.git
+# Install descartes - Geopandas requirement
+pip install descartes 
+# Install rasterstats
+pip install git+git://github.com/perrygeo/python-rasterstats.git
+# Install 
+pip install rasterio
 
 # Process complete
 
