e373ec61124f8e359d1a30930d5767a4bc014d40
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2019-12-02 12:49:58 -0800

    Update gis-colab.sh

--- gis-colab.sh
+++ gis-colab.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-# install miniconda
+# Install Miniconda
 
-echo "Installing conda"
+echo "Installing Miniconda"
+
+# Download installer for Linux 64-bit machine (another shell script)
 
 wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh
 
+# Make the installer executable as a program
+
 chmod +x Miniconda3-4.5.4-Linux-x86_64.sh
+
+# Run the installer program
 
 bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local
 
+# Install geospatial packages
 
-# install geospatial packages
+echo "Installing GIS libraries"
+
+# Use conda package manager to install geospatial Python libraries (and a visualization library) from conda-forge channel
 
 conda install -y --prefix /usr/local -c conda-forge  geopandas rasterstats matplotlib
+
+# Process complete
+
+echo "Your GIS workstation is ready!"
