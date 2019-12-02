6f2badd2e2058306a53ed14897a3c7f9ad303197
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2019-12-02 00:16:39 -0800

    Update gis-colab.sh

--- gis-colab.sh
+++ gis-colab.sh
@@ -14,4 +14,5 @@ bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local
 # install geospatial packages
 
 conda install -y --prefix /usr/local \
-                -c conda-forge rasterstats -c conda-forge geopandas 
+                -c conda-forge rasterstats \ 
+                geopandas 
