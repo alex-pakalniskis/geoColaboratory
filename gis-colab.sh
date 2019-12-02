38ed753e8633ed43c2913f85d549c28ea4a69439
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2019-12-02 00:12:50 -0800

    Add files via upload

---
+++ gis-colab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install miniconda
+
+echo "Installing conda"
+
+wget https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh
+
+chmod +x Miniconda3-4.5.4-Linux-x86_64.sh
+
+bash ./Miniconda3-4.5.4-Linux-x86_64.sh -b -f -p /usr/local
+
+
+# install geospatial packages
+
+conda install -y --prefix /usr/local \
+                -c conda-forge rasterstats -c conda-forge geopandas 
