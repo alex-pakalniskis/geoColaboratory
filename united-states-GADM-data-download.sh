dba6e0f45857195c482efae32eae93db899f09d4
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2019-12-03 19:33:32 -0800

    Update united-states-GADM-data-download.sh

--- united-states-GADM-data-download.sh
+++ united-states-GADM-data-download.sh
@@ -5,7 +5,7 @@ echo "Downloading GADM data for United States"
 wget https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_USA_shp.zip
 
 # Unzip data
-Unzip gadm36_USA_shp.zip
+unzip gadm36_USA_shp.zip
 
 # Inform user of download completion
 echo "Download complete"
