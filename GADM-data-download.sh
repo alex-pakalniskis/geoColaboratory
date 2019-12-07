6259f4ad4030c80ae6ca9c77bfb3f8205893e2b6
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2019-12-07 14:31:07 -0800

    Update GADM-data-download.sh

--- GADM-data-download.sh
+++ GADM-data-download.sh
@@ -3,9 +3,5 @@
 echo "Downloading Database of Global Administrative Areas GeoPackage"
 
 wget https://biogeo.ucdavis.edu/data/gadm3.6/gadm36_gpkg.zip
-
-# Unzip GADM data
 unzip gadm36_gpkg.zip
-
-# Inform user of download completion
 echo "Download complete"
